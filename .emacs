172e66db3253f8e040e0f7dd91aeba7a323893dc
massat <mail@massat.jp>
2012-02-26 22:54:52 +0900

    Add basic settings

---
+++ .emacs
@@ -0,0 +1,5 @@
+
+(add-to-list 'load-path "~/.emacs.d/lib")
+
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/inits")
---
+++ inits/00-basic.el
@@ -0,0 +1,36 @@
+;; スタートアップ時のメッセージを抑制
+(setq inhibit-startup-message t)
+
+;; バックアップしない
+(setq make-backup-files nil)
+
+;; 自動保存したファイルを削除する
+(setq delete-auto-save-files t)
+
+;; 自動セーブしない
+(setq auto-save-default nil)
+
+;; インデントはスペースで
+(setq-default indent-tabs-mode nil)
+
+;; 行数、列数を表示
+(line-number-mode t)
+(column-number-mode t)
+
+;; C-hでバックスペース
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; 一行ずつスクロール
+(setq scroll-step 1)
+
+;; 縦分割とかでも行を折り返す
+(setq truncate-partial-width-windows nil)
+
+;; アクティブでないバッファではカーソルを出さない
+(setq cursor-in-non-selected-windows nil)
+
+;; 行末の空白を削除
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
