6ec650bb2409cbfef3b1fa41de0e9e8d887bfcd7
massat <mail@massat.jp>
2013-05-30 11:11:19 +0900

    add rinari

---
+++ init/333-rinari.el
@@ -0,0 +1,7 @@
+;; Interactively Do Things (highly recommended, but not strictly required)
+(require 'ido)
+(ido-mode t)
+
+;; Rinari
+(add-to-list 'load-path "~/.emacs.d/elisp/rinari")
+(require 'rinari)
